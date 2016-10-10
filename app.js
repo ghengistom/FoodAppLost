029be155de328ea7f9627ba4a7da42959c7567c7
Tom <bernard5150@gmail.com>
2016-10-09 18:03:54 -0700

    fixed some 404 errors

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(__dirname + '/stylesheets'));
+app.use(express.static('public'));
+
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use('/', routes);
 app.use('/users', users);

55f0d699c36fe4e75c6b64419795e9aa69877ddd
Tom Bernard <ghengistom@gmail.com>
2016-10-24 03:42:44 -0700

    fixed the 500 error from not setting variable to null

--- routes/index.js
+++ routes/index.js
@@ -6,28 +6,79 @@ var router = express.Router();
 //mongoose.connect('mongodb://localhost/nodewebappdb');
 
 
+router.get('/', function(req, res, next) {
+    res.render('index', {
+       title: 'Muncher App!'
+  });
+});
+
+
 //Array to store filepaths to images
-var imagePathArray = new Array();
+//var imagePathArray = new Array();
+//var obj;
 
-
+//create a big string to append little strings to
+//so i can return the big string
+var bigImageString = "null";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/images', function(req, res, next) {
   var filesystem = require("fs");
   var results = [];
 
-  dir = './public/images' // project root directory
+// folder containing images
+  dir = './public/images'
+  //this will loop through every image
   filesystem.readdirSync(dir).forEach(function(file) {
 
-    file = dir+'/'+file;
-    //now turn into json object
-    imagePathArray.push(file);
-    console.log(file);
+    //save filepath
+    var pathToImg = dir+'/'+file;
+
+    //make a javascript object
+    var imageHolder = {
+      imgSrcHtml: pathToImg,
+      imgNameHtml: file
+    }
+
+    if(bigImageString==null)
+    {
+      bigImageString = JSON.stringify(imageHolder);
+    }
+    else
+    {
+      bigImageString.concat(individualImage);
+    }
+
+
+    //stringify the object we are on
+    var individualImage = JSON.stringify(imageHolder);
+
+    console.log(individualImage);
+
+    //append this to big ass stringify
+
+
+    //take this beast send it to client parsed out
+
+
+    //obj = JSON.stringify(file);
+    //console.log(obj);
+    //store each path into an array
+    //imagePathArray.push(obj);
+    //need to convert each index/path into json object
+  //  console.log(file);
   });
+  console.log(bigImageString);
 
-  //res.render('costumes', { title: 'Costumes' });
+  res.json(JSON.parse(bigImageString));
 
-  res.render('index', { title: 'Muncher App!' });
+
+//inside routes file create a get method at uri"/images"
+//	inside get method
+//		call function to get array of json objects
+
+
+
 });
 
 
